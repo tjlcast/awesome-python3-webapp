dad6c4c6c7a4a03ef2999463bf7aaf2685ff42d6
tjlcast <sx-david@visualbusiness.com>
2016-07-14 00:10:46 +0800

    orm
    add some code of ORM

---
+++ www/static/Field.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+__author__ = 'tangjialiang'
+
+class Field(object):
+    def __init__(self, name, column_type):
+        self.name = name
+        self.column_type = column_type
+
+    def __str__(self):
+        return '<%s:%s>' % (self.__class__.__name__, self.name)
+
+
+class StringField(Field):
+    def __init__(self, name):
+        super(StringField, self).__init__(name, 'varchar(100)')
+
+
+class IntegerField(Field):
+    def __init__(self, name):
+        super(IntegerField, self).__init__(name, 'bigint')
+
+
+class ModelMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+        mappings = dict()
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                mappings[k] = v
+        for k in mappings.keys():
+            attrs.pop(k)
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = name
+        return type.__new__(cls, name, bases, attrs)
+
+
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def save(self):
+        field = []
+        param = []
+        args = []
+        for k, v in self.__mappings__.items():
+            field.append(v.name)
+            param.append('?')
+            args.append(getattr(self, k, None))
+        sql = 'insert into %s () values ()' % (self.name, ''.join(field), ',',join(param))
+        #print('SQL: %s' % sql)
+        #print
+
+if __name__ == '__main__':
+    pass
