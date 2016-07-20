0691df84028933553181bb03d985b22e62ba70b1
tjlcast <sx-david@visualbusiness.com>
2016-07-20 23:16:36 +0800

    2016/7/20

--- www/main/app.py
+++ www/main/app.py
@@ -24,6 +24,47 @@ def init(loop):
     return srv
 
 
+class RequestHandle(object):
+    def __init__(self, app, fn):
+        self._app = app
+        self._func = fn
+
+    @asyncio.coroutine
+    def __call__(self, request):
+        r = yield from self._func(**kw)
+        return r
+
+def add_route(app, fn):
+    method = getattr(fn, '__methode__', None)
+    path = getattr(fn, '__path__', None)
+
+    if method is None or path is None:
+        raise ValueError('@get or @post is not defined in %s.' % str(fn))
+    if not asyncio.iscoroutine(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ','.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandle(app, fn))
+
+
+def add_route(app, module_name):
+    n = module_name.rfind('.')
+    if n == -1:
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__path__', None)
+            if method and path:
+                add_route(app, fn)
+    pass
+
 #=========================================about get and post======================================================
 def get(path):
     def decorator(func):
